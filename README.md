606466cd0f313c2a160f7db633c3e47277ee636f
Georges Boris <georgesboris@gmail.com>
2017-08-13 12:13:23 -0300

    initial setup

--- README.md
+++ README.md
@@ -4,7 +4,39 @@
 [![npm package][npm-badge]][npm]
 [![Coveralls][coveralls-badge]][coveralls]
 
-Describe validate-properties here.
+Validates the properties of a passed object using a minimalistic API.
+
+```javascript
+import validate from 'validate-properties'
+
+// checks if the object has both 'a' and 'b' properties.
+validate({}, ['a', 'b'])
+
+// checks if the object has at least one of the 'a' or 'b' properties.
+validate({}, [['a', 'b']])
+
+// checks if the object 'a' property is equal to it's 'b' property.
+validate({}, [({a , b}) => a === b])
+
+// it accepts an array so this can all be sequenced
+validate({}, [
+  // it must have an 'a' prop
+  'a',
+  // it must have at least one of the 'b' or 'c' prop
+  ['b', 'c'],
+  // it's 'd' prop must be equal to the letter 'd'
+  ({ d }) => d === 'd'
+]);
+
+// it returns a boolean by default,
+// but it can also return an array with the validations that did not pass
+// this is useful for debugging or for displaying errors on the UI
+const errors = validate({}, ['a'], true)
+
+// in the above case the errors array would be equal to:
+// [{ index: 0, validation: 'a' }]
+
+```
 
 [build-badge]: https://img.shields.io/travis/user/repo/master.png?style=flat-square
 [build]: https://travis-ci.org/user/repo
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,9 +1,73 @@
-import expect from 'expect'
+import expect from 'expect';
+import validate from 'src/index';
 
-import message from 'src/index'
+describe('without required props', function () {
+  
+  it('should return true if no props are required', function () {
+    const result = validate({});
+    expect(result).toEqual(true);
+  });
 
-describe('Module template', () => {
-  it('displays a welcome message', () => {
-    expect(message).toContain('Welcome to validate-properties')
-  })
-})
+});
+
+describe('with required props', function () {
+
+  describe('passing prop strings', function () {
+    
+    it('should return true if all required props are present', function () {
+      const result = validate({ prop: true }, ['prop']);
+      expect(result).toEqual(true);
+    });
+    
+    it('should return false if any required prop is not present', function () {
+      const result = validate({ prop: true }, ['otherProp']);
+      expect(result).toEqual(false);
+    });
+
+  });
+  
+  describe('passing prop arrays', function () {
+    
+    it('should return true if any of the props is present', function () {
+      const result = validate({ prop: true }, [['prop', 'anotherProp']]);
+      expect(result).toEqual(true);
+    });
+
+    it('should return false if none of the props are present', function () {
+      const result = validate({ prop: true }, [['otherProp', 'anotherProp']]);
+      expect(result).toEqual(false);
+    });
+
+  });
+
+  describe('passing prop functions', function () {
+
+    it('should return true if the validation function returns true', function () {
+      const result = validate({ prop: true }, [(props) => props.prop === true]);
+      expect(result).toEqual(true);
+    });
+
+    it('should return false if the validation function returns false', function () {
+      const result = validate({ prop: true }, [(props) => props.prop === false]);
+      expect(result).toEqual(false);
+    });
+
+  });
+
+});
+
+describe('with response as array', function () {
+  
+  it('should return an empty array if all required props are present', function () {
+    const result = validate({ prop: 'prop' }, ['prop'], true);
+    expect(result).toBeAn('array');
+    expect(result.length).toEqual(0);
+  });
+
+  it('should return an array of invalid props if any condition is not met', function () {
+    const result = validate({ prop: 'prop' }, ['prop', 'propA', 'propB'], true);
+    expect(result).toBeAn('array');
+    expect(result.length).toEqual(2);
+  });
+
+});
